b6e59ed3382e2f975d85fbbdfca18a1ef422be81
Jay Miller <kjaymiller@gmail.com>
2023-07-14 11:33:50 -0700

    Linting and testing with GitHub Actions.
    Also added a devcontainer for VSCode.

--- src/render_engine_tailwindcss/plugins.py
+++ src/render_engine_tailwindcss/plugins.py
@@ -1,4 +1,5 @@
 from render_engine.hookspecs import hook_impl
+from render_engine.site import Site
 import logging
 import pytailwindcss
 import pathlib
@@ -9,11 +10,14 @@ def parse_css_files(
         output_path: str|pathlib.Path,
         ) -> None:
 
-    """Parses the CSS files in the directory and creates a file for each in the output_path"""
+    """Parses CSS files in directory and create a file for each in the output_path"""
     print(list(pathlib.Path(static_path).rglob("**/*.css")))
     for file in pathlib.Path(static_path).rglob("**/*.css"):
         logging.info(f"Running Tailwind on {file}")
-        output_file = pathlib.Path(output_path).joinpath(static_path).joinpath(file.relative_to(static_path)).absolute()
+        output_file = pathlib.Path(output_path) \
+            .joinpath(static_path) \
+            .joinpath(file.relative_to(static_path)) \
+            .absolute()
         pytailwindcss.run(
             auto_install=True,
             tailwindcss_cli_args=[
@@ -26,6 +30,6 @@ def parse_css_files(
 
 class TailwindCSS:
     @hook_impl
-    def post_build_site(site: "Site") -> None:
+    def post_build_site(site: Site) -> None:
         print("running_post_build_site")
         parse_css_files(site.static_path, site.output_path)
