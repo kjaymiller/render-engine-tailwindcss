d245b2c048804ffc6c3a0ee749e9bd2a342c7fdf
Jay Miller <kjaymiller@gmail.com>
2023-12-28 12:58:01 -0500

    Adds pre_build_site hook to add tailwind_config_path (#5)

--- src/render_engine_tailwindcss/plugins.py
+++ src/render_engine_tailwindcss/plugins.py
@@ -1,4 +1,4 @@
-from render_engine.hookspecs import hook_impl
+from render_engine.plugins import hook_impl
 from render_engine.site import Site
 import logging
 import pytailwindcss
@@ -8,6 +8,7 @@ default_settings = {
     "static_path": "static",
     "output_path": "output",
     "tailwindcss_cli_input_file": ["tailwind.css"],
+    "tailwindcss_config_file": "tailwind.config.js",
 }
 
 def parse_css_files(
@@ -42,6 +43,21 @@ def parse_css_files(
 
 class TailwindCSS:
     """A plugin that runs TailwindCSS on the static files."""
+    default_settings = default_settings
+
+    @hook_impl
+    def pre_build_site(site: "Site") -> None:
+        tailwind_config_file = site.site_settings['TailwindCSS']['tailwindcss_config_file']
+        if not (config_path:=pathlib.Path(tailwind_config_file)).exists():
+            config_path.write_text("""/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['output/**/*.{html, js}'],
+    {% if plugins %}
+    plugins: [{{","|join(plugins)}}]
+    {% endif %}
+}""")
+            
+            
     @hook_impl
     def post_build_site(site: "Site") -> None:
         logging.debug("running_post_build_site")
